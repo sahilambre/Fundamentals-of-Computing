fc6fd6627fdac37d059b60a5377d3e5482b838e5
Sahil Ambre <59484827+sahilambre@users.noreply.github.com>
2022-11-03 15:53:54 -0400

    update hw3.py
    Dictionary inverter HW3 Q1

--- Code/Homework/hw3.py
+++ Code/Homework/hw3.py
@@ -1,1 +1,22 @@
+#Sahil Mukesh Ambre
+#CWID: 20015751
 
+# Question 1
+def spiral(L):
+    pass
+
+# Question 2: Write a dictionary inverter
+
+def inverter(D):
+    new_D = {}
+    for key, value in D.items():
+        if new_D.get(value):
+            new_D[value] = [new_D[value]] + [key]
+        else:
+            new_D[value] = key
+    return new_D
+
+print(inverter({1:'a', 2:'b'}))
+
+def twoSum(L,t):
+    pass
