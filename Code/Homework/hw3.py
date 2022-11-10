0c390a2995c7d86a6b815f002023a31dda75f9fc
Sahil Ambre <59484827+sahilambre@users.noreply.github.com>
2022-11-09 19:38:13 -0500

    add hw3.py
    complete hw3.py

--- Code/Homework/hw3.py
+++ Code/Homework/hw3.py
@@ -1,57 +1,60 @@
 #Sahil Mukesh Ambre
 #CWID: 20015751
 
-# Question 1
-def spiralOrder(L):
-    left, right = 0, len(L[0]) - 1
-    up, down = 0, len(L) - 1
-    ans = []
-    step = 0
-    while left <= right and up <= down:
-        if step % 4 == 0:
-            for i in range(left, right + 1):
-                ans.append(L[up][i])
-            up += 1
-        elif step % 4 == 1:
-            for i in range(up, down + 1):
-                ans.append(L[i][right])
-            right -= 1
-        elif step % 4 == 2:
-            for i in range(right, left - 1, -1):
-                ans.append(L[down][i])
-            down -= 1
-        elif step % 4 == 3:
-            for i in range(down, up - 1, -1):
-                ans.append(L[i][left])
-            left += 1
-        step += 1
-    return ans
+# Question 1: Spiral 
 
-print(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
+def spiral(L):
+    up_pointer, down_pointer = 0, len(L) - 1
+    left_pointer, right_pointer = 0, len(L[0]) - 1
+    
+    step = 0
+    answer = []
+    
+    while left_pointer <= right_pointer and up_pointer <= down_pointer:
+        if step % 4 == 0:
+            for i in range(left_pointer, right_pointer + 1):
+                answer.append(L[up_pointer][i])
+            up_pointer = up_pointer + 1
+        elif step % 4 == 1:
+            for i in range(up_pointer, down_pointer + 1):
+                answer.append(L[i][right_pointer])
+            right_pointer = right_pointer - 1
+        elif step % 4 == 2:
+            for i in range(right_pointer, left_pointer - 1, -1):
+                answer.append(L[down_pointer][i])
+            down_pointer = down_pointer - 1
+        elif step % 4 == 3:
+            for i in range(down_pointer, up_pointer - 1, -1):
+                answer.append(L[i][left_pointer])
+            left_pointer = left_pointer + 1
+        step = step + 1
+    
+    return answer
+
+# print(spiral([[1,2,3],[4,5,6],[7,8,9]]))
 
 # Question 2: Write a dictionary inverter
 
 def inverter(D):
-    new_D = {}
+    new_Dict = {}
     for key, value in D.items():
-        # if new_D.get(value):
-        if value in new_D:
-            new_D[value] = [new_D[value]] + [key]
+        if new_Dict.get(value):
+        #if value in new_D:
+            new_Dict[value] = [new_Dict[value]] + [key]
         else:
-            new_D[value] = key
-    return new_D
+            new_Dict[value] = key
+    return new_Dict
 
-print(inverter({1:'a', 2:'b'}))
+# print(inverter({1:'a', 2:'b'}))
+# print(inverter({1:'a', 2:'b', 3:'a'}))
 
-def twoSum(L,t):
-    pass
-
-#Q3: Matrix Multiplication
+# Question 3: Matrix Multiplication
 
 def matrixMultiply(A, B):
-    assert len(A[0]) == len(B),'enter correct matrix'
+    assert len(A[0]) == len(B),'Incorrect Matrix Dimesions'
 
-    ans = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+    ans = [[0 for X in range(len(B[0]))] 
+        for X in range(len(A))]
 
     for i in range(len(A)):
         for j in range(len(B[0])):
@@ -60,4 +63,20 @@ def matrixMultiply(A, B):
 
     return ans
 
-print(matrixMultiply([[1,2,3],[4,5,6]],[[1,2],[3,4]]))
+# print(matrixMultiply([[1,2,3],[4,5,6]],[[1,2],[3,4], [5,6]]))
+
+# Question 4: Two Sum
+
+def twoSum(L, t):
+    answer = []
+    new_dict = {}
+    
+    for s in range(len(L)):
+        if t - L[s] in new_dict:
+            answer.append(tuple((L[new_dict.get(t - L[s])], L[s])))
+
+        new_dict[L[s]] = s
+
+    return answer
+
+# print(twoSum([1,2,3,4,4,3,4,5], 6))
--- Code/Random/hello.py
+++ Code/Random/hello.py
@@ -1,1 +1,6 @@
 print("Hello world")
+
+def spiral(L):
+    return len(L) -1 
+
+print(spiral([[1,2,3], [4,5,6], [7,8,9]]))
